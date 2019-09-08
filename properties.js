ff2ceb84e4c4d7273a7796dbf42e758b2eda1093
Damian Hills <damski@gmail.com>
2019-09-08 19:14:40 +1000

    t

--- properties.js
+++ properties.js
@@ -19,5 +19,13 @@ AKIT_PROPS = {
 
   },
 
+  markers: [
+
+      { name: '1',url:'https://cdn.glitch.com/dcb49c1d-9b9c-4579-ae0e-2bc85f7c585a%2Fpattern-marker.patt?v=1567390421008' },
+      { name: '2',url:'https://cdn.glitch.com/dcb49c1d-9b9c-4579-ae0e-2bc85f7c585a%2Fpattern-marker2.patt?v=1567393376448' },
+      //   { name: 'your_marker_name' },
+
+  ]
+
 
 }
