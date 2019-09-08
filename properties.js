dbd8b4f825b32e0272f23f4fefbf903aa3b07f0d
Damian Hills <damski@gmail.com>
2019-09-08 15:15:48 +1000

    init

---
+++ properties.js
@@ -0,0 +1,23 @@
+
+
+AKIT_PROPS = {
+
+  ///////////////////////////////////////////////////////////basic
+  platform: 'arjs',
+
+  '8wall': {
+
+    key:''
+
+  },
+
+
+  arjs: {
+
+    cameraParametersUrl: '/assets/platform/arjs/camera_para.dat',
+  //  patternUrl: APP.markerDirectory,
+
+  },
+
+
+}
